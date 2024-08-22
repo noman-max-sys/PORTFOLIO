a01d0f39239de53c4a96d3ef72d5493c54ac967b
NOUMAN SHAFQAT <noumanchoudary9@gmail.com>
2024-08-22 08:51:17 -0700

    build

---
+++ src/Details.js
@@ -0,0 +1,163 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.png";
+import projectImage2 from "./assets/projects/project2.png";
+import projectImage3 from "./assets/projects/project3.png";
+import projectImage4 from "./assets/projects/project4.png";
+import projectImage5 from "./assets/projects/project5.png";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "NOUMAN CHAUDHARY",
+  tagline: "I build things for web",
+  img: profile,
+  about: `I am currently pursuing a my Internship at SYSTEMS ltd, where I am gaining a solid foundation in software engineering principles and programming languages. Concurrently, I am actively engaged as a Frontend Developer, where I apply my skills to create user-friendly interfaces and interactive experiences. In this role, I collaborate with teams to design and implement visually appealing and functional web applications. My experience as a Frontend Developer has honed my abilities in HTML, CSS, and JavaScript,React Js and its Libraries like MUI allowing me to contribute effectively to projects and deliver high-quality solutions. Through my education and practical experience, I am continually enhancing my technical proficiency and contributing to the dynamic field of software development.`};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/nouman-chaudhary-079362168//",
+  github: "https://github.com/noman-max-sys/",
+  twitter: "https://www.instagram.com/nouman_chaudhary1?igsh=MWlkdTh5YWNsNmx6dw==/",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "IT Support",
+    Company: `Pro Class Consultants`,
+    Location: "lahore",
+    Type: "Full Time",
+    Duration: "Sep 2022 - Dec 2023",
+  },
+  {
+    Position: "FREELANCER",
+    Company: `FIVER `,
+    Location: "REMOTE",
+    Type: "WEB ,ADX,ADSENSE Services",
+    Duration: "Jan 2024 - MAY 2024",
+  },
+  {
+    Position: "Internship",
+    Company: `SYSTEM LIMITED`,
+    Location: "Lahore Phase 8",
+    Type: "Internship",
+    Duration: "June 2024 - Sep 2024",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "BS Software Engineering",
+    Company: "Superior University",
+    Location: "Lahore Raiwind",
+    Duration: "June 2020 - June 2024",
+  },
+  {
+    Position: "Fsc Pre Engineering",
+    Company: `Superior College`,
+    Location: "Juhrabad",
+    Duration: " 2018 - 2020 ",
+  },
+  {
+    Position: "Matric",
+    Company: "The Educators Project Of BECONHOUSE",
+    Location: "Juhrabad",
+    Duration: "June 2016 - June 2018",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Pro Class Constants Website",
+    image: projectImage1,
+    description: `I BUILD THIS SITE FOR MY CLIENT IN LAHORE`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/noman-max-sys",
+  },
+  {
+    title: "PCC WEB APP",
+    image: projectImage2,
+    description: `WE ARE BUILDING WEB APP FORR PCC BUT ITS NOT LIVE YET BUT WEBSITE IS LIVE `,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/noman-max-sys",
+  },
+  {
+    title: "AMAZON POWER BI DASHBOARD",
+    image: projectImage3,
+    description: `THIS DASBOARD IS PERRFECT FOR LEARNING OF POWER BI`,
+    techstack: "POWER BI",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/noman-max-sys",
+  },
+  {
+    title: "SYSTEMS LTD POWER BI DASHBOARD",
+    image: projectImage4,
+    description: `PROJECT IS ABOUT SYSTEMS SUB COMPANIES`,
+    techstack: "POWER BI",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/noman-max-sys",
+  },
+  {
+    title: "PERSONAL PORTFOLIO",
+    image: projectImage5,
+    description: `DETAILED PORTFOLIO`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/noman-max-sys",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "noumanchoudary9@gmail.com",
+  phone: "+923061564265",
+};
